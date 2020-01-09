b4ead5953b26b66e2709e8cd0e7c3d3d46a95b3d
AlexJoo2003 <alexanderalnoumeh@gmail.com>
2020-01-09 13:18:22 +0100

    "Hello World" Added

--- Program.cs
+++ Program.cs
@@ -6,22 +6,8 @@ namespace GitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
 
-            Console.Write("2 + 2 = ");
-
-            string inp = Console.ReadLine();
-
-            if (inp == "4")
-            {
-                Console.WriteLine("You are right");
-            }
-            else
-            {
-                Console.WriteLine("WRONG");
-            }
-            Console.WriteLine("Adrian is a pussy");
-            Console.WriteLine("Please work!!!");
         }
     }
 }
