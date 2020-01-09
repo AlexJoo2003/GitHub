0175cff9d81db8b64c9f5e178278b7b973616520
AlexJoo2003 <alexanderalnoumeh@gmail.com>
2020-01-09 13:03:19 +0100

    Added an if statement

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.Write("2 + 2 = ");
+
+            string inp = Console.ReadLine();
+
+            if (inp == "4")
+            {
+                Console.WriteLine("You are right");
+            }
+            else
+            {
+                Console.WriteLine("WRONG");
+            }
         }
     }
 }
